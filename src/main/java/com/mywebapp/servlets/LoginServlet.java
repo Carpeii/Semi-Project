4350cccd89da04b9aabfb79de49d82210ebccb58
Yijae <lullulalla9711@gmail.com>
2024-07-23 12:47:19 +0900

    [edit]login 수정

--- src/main/java/com/mywebapp/servlets/LoginServlet.java
+++ src/main/java/com/mywebapp/servlets/LoginServlet.java
@@ -5,15 +5,16 @@ import org.example.semiproject.HelloServlet;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/auth/login")
-public class LoginServlet extends HelloServlet {
+@WebServlet("/login")
+public class LoginServlet extends HttpServlet {
     @Override
-    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/service/main.jsp").forward(request, response);
     }
 
     @Override
@@ -21,13 +22,40 @@ public class LoginServlet extends HelloServlet {
         String id = req.getParameter("id");
         String pw = req.getParameter("pw");
 
-        if(id != null && pw != null) { // 공백이 아니고 값이 다 있는 경우
-            // if(req.getParameter("guestLogin") != null) {
-                //resp.sendRedirect(req.getContextPath() + "/auth/guestLoginOk.jsp");
-                req.getRequestDispatcher("/guestLoginOk.jsp").forward(req, resp);
-            } else if(req.getParameter("hostLogin") != null) {
-                //resp.sendRedirect(req.getContextPath() + "/auth/hostLoginOk.jsp");
-                req.getRequestDispatcher("/hostLoginOk.jsp").forward(req, resp);
+        String guestLogin = req.getParameter("guestLogin");
+        String hostLogin = req.getParameter("hostLogin");
+
+        if(guestLogin != null) { // 게스트 로그인 버튼을 눌렀고,
+            if (id != null && pw != null) { // 공백이 아니고 값이 다 있는 경우
+                // db연결해서 아이디와 비밀번호가 일치하는지 확인하기
+                // 아직 db 없어서 예시로
+                if (id.equals("test") && pw.equals("1234")) {
+                    req.getSession().setAttribute("id", id);
+                    resp.sendRedirect(req.getContextPath() + "/service/main.jsp");
+                } else {
+                    req.setAttribute("errMag", "아이디와 비밀번호를 확인하세요");
+                    req.getRequestDispatcher("/auth/loginMain.jsp").forward(req, resp);
+                }
+                // req.getRequestDispatcher("/guestLoginOk.jsp").forward(req, resp);
+            } else {
+                req.setAttribute("errMag", "아이디와 비밀번호를 모두 채워주세요.");
+                req.getRequestDispatcher("/auth/loginMain.jsp").forward(req, resp);
+            }
+        } else if (hostLogin != null) {
+            if (id != null && pw != null) { // 공백이 아니고 값이 다 있는 경우
+                // db연결해서 아이디와 비밀번호가 일치하는지 확인하기
+                if (id.equals("test") && pw.equals("1234")) {
+                    req.getSession().setAttribute("id", id);
+                    resp.sendRedirect(req.getContextPath() + "/service/hostMain.jsp");
+                } else {
+                    req.setAttribute("errMag", "아이디, 비밀번호를 확인하세요");
+                    req.getRequestDispatcher("/auth/loginMain.jsp").forward(req, resp);
+                    // 로그인처럼 보안이 필요한 이동은 RequestDispatcher 써야함
+                    // 사용자 화면 url에서 이동이 보이지 않음
+                }
+                // req.getRequestDispatcher("/guestLoginOk.jsp").forward(req, resp);
+            } else {
+                req.getRequestDispatcher("/auth/loginMain.jsp").forward(req, resp);
             }
         }
     }
--- src/main/webapp/jsp/auth/loginMain.jsp
+++ src/main/webapp/jsp/auth/loginMain.jsp
@@ -11,20 +11,11 @@
     <title>loginMain.jsp</title>
 </head>
 <body>
-<%
-    String errMsg = (String)request.getAttribute("errMsg");
-    String id = request.getParameter("id");
-    String pw = request.getParameter("pw");
-    //if(errMsg==null) errMsg="";
-
-    // 에러 메세지 띄우기
-//    if(id == null || pw == null) {}
-%>
-<h1>로그인</h1>
-<form action="${cp}/auth/login" method="post">
+<h1>Login</h1>
+<form action="${pageContext.request.contextPath}/login" method="post">
     아이디: <input type="text" name="id"><br>
     비밀번호: <input type="password" name="pw"><br>
-    <!-- <div><%=errMsg %></div> -->
+    <div>${errMsg}</div>
     <input type="submit" name="guestLogin" value="게스트 로그인">
     <input type="submit" name="hostLogin" value="호스트 로그인">
 </form>
