4e28e6e4243ea3c894712dcfdf98c5eaba2f361b
Carpeii <52017539+Carpeii@users.noreply.github.com>
2024-07-24 16:44:13 +0900

    [fix] RoomPriceDao쪽도 변수명 수정

--- src/main/java/com/mywebapp/dao/RoomPriceDao.java
+++ src/main/java/com/mywebapp/dao/RoomPriceDao.java
@@ -15,7 +15,7 @@ public class RoomPriceDao {
 
         try {
             connection = JdbcUtil.getCon();
-            String sql = "INSERT INTO room_price (room_id, rent_price, long_term, long_term_discount, early_check_in, early_check_in_discount, maintenance_bill, maintenance_bill_discount, electricity, water, gas, internet, cleaning_fee, refund_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO room_price (room_id, rent_price, long_term, long_term_discount, early_check_in, early_check_in_discount, maintenance_bill, maintenance_bill_detail, electricity, water, gas, internet, cleaning_fee, refund_type) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
             stmt = connection.prepareStatement(sql);
             stmt.setLong(1, roomPrice.getRoomId());
             stmt.setInt(2, roomPrice.getRentPrice());
@@ -24,7 +24,7 @@ public class RoomPriceDao {
             stmt.setInt(5, roomPrice.getEarlyCheckIn());
             stmt.setInt(6, roomPrice.getEarlyCheckInDiscount());
             stmt.setInt(7, roomPrice.getMaintenanceBill());
-            stmt.setInt(8, roomPrice.getMaintenanceBillDiscount());
+            stmt.setInt(8, roomPrice.getMaintenanceBillDetail());
             stmt.setBoolean(9, roomPrice.isElectricity());
             stmt.setBoolean(10, roomPrice.isWater());
             stmt.setBoolean(11, roomPrice.isGas());
