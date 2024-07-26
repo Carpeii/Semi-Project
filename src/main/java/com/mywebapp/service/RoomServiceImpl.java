9766885a9125776a2993978c3c2b94f07aab0acb
jaeeun <rosa2070@naver.com>
2024-07-26 17:14:25 +0900

    [add] service 패키지에 RoomService, RoomServiceImple 추

---
+++ src/main/java/com/mywebapp/service/RoomServiceImpl.java
@@ -0,0 +1,40 @@
+package com.mywebapp.service;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.mywebapp.dao.RoomDao;
+import com.mywebapp.dto.RoomListItemDto;
+
+public class RoomServiceImpl implements RoomService {
+	
+	private RoomDao roomDao = new RoomDao();
+
+	@Override
+	public List<RoomListItemDto> getRoomList(int pageNum, int pageSize) {
+		int offset = (pageNum - 1) * pageSize;
+		return roomDao.findAllRoomListItems(offset, pageSize);
+	}
+
+	@Override
+	public int getTotalRoomCount() {
+		return roomDao.getTotalRoomCount();
+	}
+
+	@Override
+	public Map<String, Object> calculatePagination(int pageNum, int pageSize, int totalCount) {
+		Map<String, Object> paginationInfo = new HashMap<>();
+		int pageCount = (int) Math.ceil((double) totalCount / pageSize);
+		int startPage = ((pageNum - 1) / 10) * 10 + 1;
+		int endPage = Math.min(startPage + 9, pageCount);
+		
+		paginationInfo.put("pageCount", pageCount);
+		paginationInfo.put("startPage", startPage);
+		paginationInfo.put("endPage", endPage);
+		paginationInfo.put("pageNum", pageNum);
+		
+		return paginationInfo;
+	}
+	
+}
