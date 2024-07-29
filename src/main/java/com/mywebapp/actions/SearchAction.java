f5ddbf890449b63df9b133bff4b9bc01af330281
poxi00 <poix00@gamil.com>
2024-07-29 14:33:07 +0900

    [add]SearchAction 클래스 추가 + SearchController코드 이동
    SearchController의 db연동 코드 action클래스로 이동

---
+++ src/main/java/com/mywebapp/actions/SearchAction.java
@@ -0,0 +1,48 @@
+package com.mywebapp.actions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.mywebapp.dao.RoomDao;
+import com.mywebapp.model.Room;
+
+public class SearchAction implements Action {
+
+	@Override
+	public void execute(HttpServletRequest req, HttpServletResponse resp) {
+		String searchword = req.getParameter("searchword");
+		System.out.println(searchword);
+		
+	RoomDao dao = new RoomDao(); 
+	ArrayList<Room> rooms = (ArrayList<Room>)dao.search(searchword);
+	for(Room r : rooms) {
+		int price = r.getRoomPrice().getRentPrice();
+		int approve = r.getApprove();
+		String address = r.getJibunAddress();
+		String addressDetail = r.getAddressDetail();
+		
+		System.out.println(approve +"   " + address +" " +addressDetail+" " + price );
+		
+	}
+	//클라이언트에 보여줄 값으로 가공 후 dto에 넣어서 보내기
+	req.setAttribute("rooms", rooms);
+	
+	try {
+		
+		req.getRequestDispatcher("/jsp/service/search.jsp").forward(req, resp);
+	} catch (ServletException e) {
+		
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	}
+}
+
+
--- src/main/java/com/mywebapp/controllers/user/SearchController.java
+++ src/main/java/com/mywebapp/controllers/user/SearchController.java
@@ -13,30 +13,18 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.apache.jasper.tagplugins.jstl.core.ForEach;
 
+import com.mywebapp.actions.Action;
+import com.mywebapp.actions.SearchAction;
 import com.mywebapp.dao.RoomDao;
 import com.mywebapp.model.Room;
 import com.mywebapp.model.RoomPrice;
 @WebServlet("/host/search")
 public class SearchController  extends HttpServlet  {
 	@Override
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		req.setCharacterEncoding("utf-8");
-		String searchword = req.getParameter("searchword");
-		System.out.println(searchword);
-		
-	RoomDao dao = new RoomDao(); 
-	ArrayList<Room> rooms = (ArrayList<Room>)dao.search(searchword);
-	for(Room r : rooms) {
-		int price = r.getRoomPrice().getRentPrice();
-		int approve = r.getApprove();
-		String address = r.getJibunAddress();
-		String addressDetail = r.getAddressDetail();
-		
-		System.out.println(approve +"   " + address +" " +addressDetail+" " + price );
-		
-	}
-	req.setAttribute("rooms", rooms);
-	req.getRequestDispatcher("/jsp/service/search.jsp").forward(req, resp);
+		Action action = new SearchAction();
+		action.execute(req, resp);
 	}
 
 }
