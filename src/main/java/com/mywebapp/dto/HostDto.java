321602ecd7ff467fa979670f1a207d7076cffa7c
Yijae <lullulalla9711@gmail.com>
2024-07-29 12:38:43 +0900

    [add] Dto 수정
    Controller에서 쓰기 위해 getter-setter 추가

--- src/main/java/com/mywebapp/dto/HostDto.java
+++ src/main/java/com/mywebapp/dto/HostDto.java
@@ -6,16 +6,48 @@ import lombok.Setter;
 @Getter @Setter
 public class HostDto {
     private long memberId;
-    private String hostName;
+    private String bankName;
     private String account;
     private String account_holder;
 
     public HostDto() {
     }
-    public HostDto(Long memberId, String hostName, String account, String account_holder) {
+    public HostDto(Long memberId, String bankName, String account, String account_holder) {
         this.memberId = memberId;
-        this.hostName = hostName;
+        this.bankName = bankName;
         this.account = account;
+        this.account_holder = account_holder;
+    }
+
+    public long getMemberId() {
+        return memberId;
+    }
+
+    public void setMemberId(long memberId) {
+        this.memberId = memberId;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setHostName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getAccount_holder() {
+        return account_holder;
+    }
+
+    public void setAccount_holder(String account_holder) {
         this.account_holder = account_holder;
     }
 }
--- src/main/java/com/mywebapp/dto/MemberDto.java
+++ src/main/java/com/mywebapp/dto/MemberDto.java
@@ -30,4 +30,70 @@ public class MemberDto {
         this.createdAt = createdAt;
         this.updatedAt = updatedAt;
     }
+
+    // Private로 선언한 변수를 밖에서 쓰기 위한 getter setter 생성
+//    public String getId() {
+//        return id;
+//    }
+//
+//    public void setId(long id) {
+//        this.id = id;
+//    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public int getMemberType() {
+        return memberType;
+    }
+
+    public void setMemberType(int memberType) {
+        this.memberType = memberType;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
 }
