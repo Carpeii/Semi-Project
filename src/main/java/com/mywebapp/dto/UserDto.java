4cd98974aa7d5a81d2645c588410aeec6e66e611
Yijae <lullulalla9711@gmail.com>
2024-07-31 17:16:21 +0900

    [add] 회원가입, 로그인 최종❗️

--- src/main/java/com/mywebapp/dto/UserDto.java
+++ src/main/java/com/mywebapp/dto/UserDto.java
@@ -1,2 +1,12 @@
-package com.mywebapp.dto;public class UserDto {
+package com.mywebapp.dto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserDto {
+    private String id;
+    private String password;
+
+    public UserDto() {}
 }
