012a8d97cb8c6b21e442fa989db38cda2476a6d2
Carpeii <52017539+Carpeii@users.noreply.github.com>
2024-07-31 15:14:52 +0900

    [fix] RoomImage 변수 추가 saveFileName

--- src/main/java/com/mywebapp/model/RoomImage.java
+++ src/main/java/com/mywebapp/model/RoomImage.java
@@ -10,8 +10,7 @@ public class RoomImage {
     private long id;
     private long roomId;
     private String imageName;
+    private String saveFileName;
     private String imagePath;
     private int imageOrder;
-
- 
 }
