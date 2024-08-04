d34a72bbcc0df5ce2e70fb510fa4cb6d4e60ebaf
Carpeii <52017539+Carpeii@users.noreply.github.com>
2024-08-05 01:19:37 +0900

    [feat] 관리자 어드민 방관리페이지 추가

--- src/main/java/com/mywebapp/controllers/admin/RoomManagementController.java
+++ src/main/java/com/mywebapp/controllers/admin/RoomManagementController.java
@@ -1,17 +1,26 @@
 package com.mywebapp.controllers.admin;
 
+import com.mywebapp.dto.UserDto;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/admin/roomManagement")
 public class RoomManagementController extends HttpServlet {
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/jsp/admin/loginMain.jsp").forward(request, response);
 
+    }
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        
+        HttpSession session = req.getSession();
+        UserDto dto = (UserDto) session.getAttribute("user");
+
     }
 }
