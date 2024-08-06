7e653ced3d8ab3338e966d9779ee180e47c07beb
Carpeii <52017539+Carpeii@users.noreply.github.com>
2024-08-06 13:17:08 +0900

    [fix] hostRoomList dto로 받아오게끔 수정

--- src/main/java/com/mywebapp/controllers/user/HostRoomListController.java
+++ src/main/java/com/mywebapp/controllers/user/HostRoomListController.java
@@ -2,6 +2,7 @@ package com.mywebapp.controllers.user;
 
 import com.mywebapp.dao.RoomDao;
 import com.mywebapp.dao.RoomDaoImpl;
+import com.mywebapp.dto.UserDto;
 import com.mywebapp.model.Room;
 
 import javax.servlet.ServletException;
@@ -9,6 +10,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
@@ -18,17 +20,19 @@ public class HostRoomListController extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // Database connection setup (use a connection pool in a real application)
-//        String hostId = req.getParameter("hostId");
-        long hostId = 1;
-//        if(hostId == null || hostId.isEmpty()) {
-//
-//            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Host ID is required");
-//            return;
-//        }
-        System.out.println(hostId);
+//        // Database connection setup (use a connection pool in a real application)
+////        String hostId = req.getParameter("hostId");
+//        long hostId;
+////        if(hostId == null || hostId.isEmpty()) {
+////
+////            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Host ID is required");
+////            return;
+////        }
+//        System.out.println(hostId);
+        HttpSession session = req.getSession();
+        UserDto user = (UserDto) session.getAttribute("user");
         RoomDao roomDao = new RoomDaoImpl();
-        List<Room> rooms = roomDao.getRoomsByHostId(hostId);
+        List<Room> rooms = roomDao.getRoomsByHostId(user.getId());
 
         req.setAttribute("rooms", rooms);
         req.getRequestDispatcher("/jsp/service/hostRoomList.jsp").forward(req, resp);
